9b0281e5d722bb39984c267086ca4c59d12d4a39
artem <a.vichuk@skbkontur.ru>
2026-03-28 11:27:16 +0300

    fix

--- internal/infrastructure/mongo/client.go
+++ internal/infrastructure/mongo/client.go
@@ -13,12 +13,16 @@ import (
 )
 
 func NewClient(ctx context.Context, cfg config.Config) (*gomongo.Client, error) {
+	dbName := url.QueryEscape(cfg.Mongo.Database)
+
 	uri := fmt.Sprintf(
-		"mongodb://%s:%s@%s:%d",
+		"mongodb://%s:%s@%s:%d/%s?authSource=%s",
 		url.QueryEscape(cfg.Mongo.User),
 		url.QueryEscape(cfg.Mongo.Password),
 		cfg.Mongo.Host,
 		cfg.Mongo.Port,
+		dbName,
+		dbName,
 	)
 
 	client, err := gomongo.Connect(ctx, options.Client().ApplyURI(uri))
